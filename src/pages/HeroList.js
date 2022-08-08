c4a7dbb37fc9dfe49f575f1f94fc375c8a68d3bb
evan <evan.hoizey@ynov.com>
2022-08-08 14:00:21 +0200

    retour en composant en classe pour montrer le rendu initial

--- src/pages/HeroList.js
+++ src/pages/HeroList.js
@@ -1,26 +1,33 @@
 import logo from '../logo.svg';
 import '../HeroList.css';
 import {Route, Link, Routes, useParams} from 'react-router-dom';
-import React, {Component, useState} from 'react';
+import React, { Component } from 'react';
 
 // import Hero from './Hero';
 
-function HeroList (props) {
-    const [error, setError] = useState(null);
-    let isLoaded = useState(false);
-    let items = useState([]);
+class HeroList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
 
-    const componentDidMount = () => {
+    componentDidMount() {
         fetch("https://gateway.marvel.com:443/v1/public/characters?limit=100&apikey=b6995d361d0c44520ec906f87e2b2cdc",{method: "GET"})
             .then(res => res.json())
             .then(
                 (result) => {
-                    isLoaded = true;
-                    let items = result.data.results;
+                    this.setState({
+                        isLoaded: true,
+                        items: result.data.results
+                    });
                 },
 
                 (error) => {
-                    setState({
+                    this.setState({
                         isLoaded: true,
                         error
                     });
@@ -28,22 +35,26 @@ function HeroList (props) {
             )
     }
 
-    if (error) {
-        return <div>Erreur : {error.message}</div>;
-    } else if (!isLoaded) {
-        return <div>Chargement…</div>;
-    } else {
-        return (
-            <ul>
-                {items.map(item => (
-                    <div key={item.id}>
-                        <p> {item.name}: { item.description } </p>
-                        <Link to={'/' + item.id}>Link to page</Link>
-                    </div>
-                ))}
-            </ul>
-        );
-    };
+    render() {
+        const { error, isLoaded, items } = this.state;
+
+        if (error) {
+            return <div>Erreur : {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Chargement…</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <div key={item.id}>
+                            <p> {item.name}: { item.description } </p>
+                            <Link to={"/" + item.id}>Link to page</Link>
+                        </div>
+                    ))}
+                </ul>
+            );
+        }
+    }
 }
 
 export default HeroList;
